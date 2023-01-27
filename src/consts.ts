abad9ed8f55a0a390423a89277b11a35af4fead4
Roberto Ramirez <robert.624@hotmail.com>
2023-01-27 17:05:13 -0500

    added contact and about pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mi blog personal';
+export const SITE_DESCRIPTION = 'Bienvenido a mi blog!';
