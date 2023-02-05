c50f1a239f19aa759bb3b42e861832630b9025c0
Roberto Ramirez <robert.624@hotmail.com>
2023-02-05 11:59:27 -0500

    changed deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://Robetron624.github.com',
+	base: "/robert-landing",
 	integrations: [mdx(), sitemap()],
 });
